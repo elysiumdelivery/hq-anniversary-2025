6de76d3151d440cbcb75c65eafc040d7534f5cb6
bon <bon@bon-v.tube>
2025-04-09 20:58:36 -0400

    page setup for review and receipt. setting up new homepage design. refining loading ux for corkboard

---
+++ js/review.js
@@ -0,0 +1,67 @@
+const MESSAGES_CSV = "../data/guestbook.csv";
+
+function getCSVData(filename, callback = undefined) {
+  Papa.parse(filename, {
+    download: true,
+    //To treat the first row as column titles
+    header: true,
+    complete: (result) => {
+      if (callback) {
+        callback(result.data);
+      }
+    },
+  });
+}
+
+function renderMessages(message_data) {
+  let messageContainer = document.getElementById("guestbook-messages");
+  for (var i = 0; i < message_data.length; i++) {
+    let message = message_data[i];
+    let messageNode = document.createElement("li");
+    messageNode.classList.add("guestbook-message");
+    let messageFromDiv = document.createElement("div");
+    let messageUserHeader = document.createElement("div");
+    let messageUserInfo = document.createElement("div");
+    let messageUserMessage = document.createElement("p");
+    let messageReviewStars = document.createElement("div");
+    let messageReviewIcon = document.createElement("div");
+    
+    messageReviewIcon.classList.add("guestbook-icon");
+    messageReviewStars.classList.add("guestbook-stars");
+
+    messageFromDiv.classList.add("guestbook-user");
+
+    if (message["Twitter"] && message["Twitter"] !== "-") {
+      let userSocial = document.createElement("a");
+      userSocial.href = `https://twitter.com/${message["Twitter"]}`;
+      userSocial.setAttribute('target', '_blank');
+      userSocial.textContent =  message["Name"];
+      messageUserHeader.append(userSocial);
+    }
+    else {
+      messageUserHeader.innerText = message["Name"];
+    }
+    messageUserMessage.innerText = message["Message"];
+
+    for (var j = 0; j < 5; j++) {
+      let messageReviewStar = document.createElement("div");
+      messageReviewStar.textContent = "★";
+      messageReviewStars.appendChild(messageReviewStar);
+    }
+
+    messageFromDiv.appendChild(messageReviewIcon);
+    messageFromDiv.appendChild(messageUserHeader);
+    messageFromDiv.appendChild(messageUserInfo);
+    messageNode.appendChild(messageReviewStars);
+    messageNode.appendChild(messageFromDiv);
+    messageNode.appendChild(messageUserMessage);
+
+    messageContainer.appendChild(messageNode);
+  }
+}
+
+async function main() {
+    getCSVData(MESSAGES_CSV, renderMessages);
+}
+
+main();
