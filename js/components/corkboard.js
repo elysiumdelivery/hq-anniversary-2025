dd54943d3f24fcfac9e2d5a4fc6ac6130872cd04
bon <bon@bon-v.tube>
2025-05-10 00:32:18 -0400

    setting up funcs for reliable runtime generation.

--- js/components/corkboard.js
+++ js/components/corkboard.js
@@ -1,9 +1,14 @@
 let DETAILS_DIALOG_A11Y = null;
 let DETAILS_DIALOG_EL = null;
+
+const SETUP = false;
+
 let msnry;
-let photos = ["Food1",  "Mem3", "Food5", "Writing1", "Mem1",  "Food3", "Writing1", "Mem5", "Food1", "Writing1", "Mem3", "Food5", "Writing1", "Mem1",  "Food3", "Mem5"]
+let photos = ["Food1", "Writing1",  "Mem3", "Food5", "Mem1",  "Food3", "Writing1", "Mem5", "Food1", "Mem1",  "Food3", "Writing1", "Mem3", "Food5", "Writing1", "Mem5", "Food5", "Mem1",  "Food3", "Writing1", "Mem5", "Food1", "Mem1",  "Food3", "Writing1", "Mem3", "Food5", "Writing1", "Mem5", "Food1", "Writing1",  "Mem3", "Food5", "Mem1",  "Food3", "Writing1", "Mem5", "Food1", "Mem1",  "Food3", "Writing1", "Mem3", "Food5", "Writing1", "Mem5", "Food5", "Mem1",  "Food3", "Writing1", "Mem5", "Food1", "Mem1",  "Food3", "Writing1", "Mem3", "Food5", "Writing1", "Mem5", "Food1", "Writing1",  "Mem3", "Food5", "Mem1",  "Food3", "Writing1", "Mem5", "Food1", "Mem1",  "Food3", "Writing1", "Mem3", "Food5", "Writing1", "Mem5", "Food5", "Mem1",  "Food3", "Writing1", "Mem5", "Food1", "Mem1",  "Food3", "Writing1", "Mem3", "Food5", "Writing1", "Mem5", "Food1", "Writing1",  "Mem3", "Food5", "Mem1",  "Food3", "Writing1", "Mem5", "Food1", "Mem1",  "Food3", "Writing1", "Mem3", "Food5", "Writing1", "Mem5", "Food5", "Mem1",  "Food3", "Writing1", "Mem5", "Food1", "Mem1",  "Food3", "Writing1", "Mem3", "Food5", "Writing1", "Mem5"]
 let singleDecos = ["eds.png", "home/polaroid1.png", "home/polaroid2.png"];
 let reusableDecos = ["star1.png", "star2.png"];
+
+const randomData = [];
 
 window.addEventListener("DOMContentLoaded", function() {   
     window.scrollTo(0, 0);
@@ -37,19 +42,30 @@ class Corkboard extends HTMLElement {
   
     connectedCallback() {
         this.role = "list";
+
         const options = {
-            rootMargin: "20%",
+            rootMargin: "100px",
             trackVisibility: true,
             threshold: 0.5,
             delay: 100,
         };
         this.observer = new IntersectionObserver(scrollIntersectionCallback, options);
-        setupEntries(this);
+
+        if (SETUP) {
+            genEntries();
+            setupEntries(this, randomData);
+        }
+        else {
+            setupEntries(this, CORKBOARD_DATA)
+        }
+
+        
         msnry = new Masonry(this, {
             // options
             itemSelector: '.corkboard-item',
             columnWidth: 200,
             initLayout: false,
+            horizontalOrder: true,
             fitWidth: true,
             gutter: 50,
             stagger: 50,
@@ -68,41 +84,14 @@ class Corkboard extends HTMLElement {
                             elem.classList.remove("anim-in");
                         }
                     }
-                    // if (elem.getAttribute("entry-type") === "art") {
-                    //     elem.imageThumb.src = `image-resize/50/corkboard-img/${elem.getAttribute("photo-key")}.png`;
-                    //     elem.imageThumb.srcset = [
-                    //         `image-resize/200/corkboard-img/${elem.getAttribute("photo-key")}.png    200w`,
-                    //         `image-resize/400/corkboard-img/${elem.getAttribute("photo-key")}.png    400w`,
-                    //     ].join(",");
-                    //     elem.imageThumb.sizes = [
-                    //         "(max-width: 450px) 200px",
-                    //         "(max-width: 850px) 400px",
-                    //         "400px"
-                    //     ].join(",");
-                    // }
-                    // else {
-                    //     elem.imageThumb.src = `image-resize/50/corkboard/writing_thumb.png`;
-                    //     elem.imageThumb.srcset = [
-                    //         `image-resize/200/corkboard/writing_thumb.png    200w`,
-                    //         `image-resize/400/corkboard/writing_thumb.png    400w`,
-                    //     ].join(",");
-                    //     elem.imageThumb.sizes = [
-                    //         "(max-width: 450px) 200px",
-                    //         "(max-width: 850px) 400px",
-                    //         "400px"
-                    //     ].join(",");
-                    // }
                     if (elem.imageThumb.complete) {
                         elem.classList.add("anim-in");
-                        // setTimeout(() => msnry.layout(), 500);
                     }
-                    // observer.unobserve(entry.target);
                 }
                 // msnry.off("layoutComplete");
             });
         });
         imagesLoaded(this).on("progress", function (instance, image) {
-            console.log(image)
             msnry.layout();
         });
     }
@@ -128,13 +117,17 @@ class CorkboardItem extends HTMLElement {
         }
 
         this.imageThumb = document.createElement("img");
-        this.imageThumb.loading = "lazy";
-        this.imageThumb.addEventListener("load", function (e) {
-            let ratio = e.target.naturalWidth / e.target.naturalHeight;
-            this.style.setProperty('aspect-ratio', ratio.toFixed(2));
-            // this.classList.add("anim-in");
-            // setTimeout(function () { msnry.appended(this); }.bind(this), 500);
-        }.bind(this))
+        // this.imageThumb.loading = "lazy";
+        if (SETUP) {
+            this.imageThumb.addEventListener("load", function (e) {
+                let ratio = e.target.naturalWidth / e.target.naturalHeight;
+                this.style.setProperty('aspect-ratio', ratio.toFixed(2));
+                // this.classList.add("anim-in");
+                let idx = Array.from(this.parentNode.children).indexOf(this);
+                randomData[idx].ratio = ratio;
+                // setTimeout(function () { msnry.appended(this); }.bind(this), 500);
+            }.bind(this))
+        }
         // this.image = document.createElement("img");
         if (this.getAttribute("entry-type") === "art") {
             this.imageThumb.src = `image-resize/50/corkboard-img/${this.getAttribute("photo-key")}.png`;
@@ -260,69 +253,58 @@ customElements.define("corkboard-item", CorkboardItem);
 function scrollIntersectionCallback (entries, observer) {
     entries.forEach((entry, i) => {
         let elem = entry.target;
-        if (entry.isIntersecting && !(elem.classList.contains("visible"))) {
-            elem.onanimationend = (event) => {
-                if (event.animationName == "corkboard-item-anim-in") {
-                    elem.classList.add("visible");
-                    elem.classList.remove("anim-in");
-                }
-            }
-            // if (elem.getAttribute("entry-type") === "art") {
-            //     elem.imageThumb.src = `image-resize/50/corkboard-img/${elem.getAttribute("photo-key")}.png`;
-            //     elem.imageThumb.srcset = [
-            //         `image-resize/200/corkboard-img/${elem.getAttribute("photo-key")}.png    200w`,
-            //         `image-resize/400/corkboard-img/${elem.getAttribute("photo-key")}.png    400w`,
-            //     ].join(",");
-            //     elem.imageThumb.sizes = [
-            //         "(max-width: 450px) 200px",
-            //         "(max-width: 850px) 400px",
-            //         "400px"
-            //     ].join(",");
+        elem.style.setProperty("content-visibility", entry.isIntersecting ? "visible" : "hidden");
+        observer.unobserve(entry.target);
+        // if (entry.isIntersecting && !(elem.classList.contains("visible"))) {
+
+            // elem.onanimationend = (event) => {
+            //     if (event.animationName == "corkboard-item-anim-in") {
+            //         elem.classList.add("visible");
+            //         elem.classList.remove("anim-in");
+            //     }
             // }
-            // else {
-            //     elem.imageThumb.src = `image-resize/50/corkboard/writing_thumb.png`;
-            //     elem.imageThumb.srcset = [
-            //         `image-resize/200/corkboard/writing_thumb.png    200w`,
-            //         `image-resize/400/corkboard/writing_thumb.png    400w`,
-            //     ].join(",");
-            //     elem.imageThumb.sizes = [
-            //         "(max-width: 450px) 200px",
-            //         "(max-width: 850px) 400px",
-            //         "400px"
-            //     ].join(",");
+            // // if (elem.getAttribute("entry-type") === "art") {
+            // //     elem.imageThumb.src = `image-resize/50/corkboard-img/${elem.getAttribute("photo-key")}.png`;
+            // //     elem.imageThumb.srcset = [
+            // //         `image-resize/200/corkboard-img/${elem.getAttribute("photo-key")}.png    200w`,
+            // //         `image-resize/400/corkboard-img/${elem.getAttribute("photo-key")}.png    400w`,
+            // //     ].join(",");
+            // //     elem.imageThumb.sizes = [
+            // //         "(max-width: 450px) 200px",
+            // //         "(max-width: 850px) 400px",
+            // //         "400px"
+            // //     ].join(",");
+            // // }
+            // // else {
+            // //     elem.imageThumb.src = `image-resize/50/corkboard/writing_thumb.png`;
+            // //     elem.imageThumb.srcset = [
+            // //         `image-resize/200/corkboard/writing_thumb.png    200w`,
+            // //         `image-resize/400/corkboard/writing_thumb.png    400w`,
+            // //     ].join(",");
+            // //     elem.imageThumb.sizes = [
+            // //         "(max-width: 450px) 200px",
+            // //         "(max-width: 850px) 400px",
+            // //         "400px"
+            // //     ].join(",");
+            // // }
+            // if (elem.imageThumb.complete) {
+            //     elem.classList.add("anim-in");
             // }
-            if (elem.imageThumb.complete) {
-                elem.classList.add("anim-in");
-                // setTimeout(() => msnry.layout(), 500);
-            }
-            observer.unobserve(entry.target);
-        }
+        // }
     });
 }
 
-function setupEntries (elem) {
+function genEntries () {
     let plusOrMinus = 1;
     let pmCount = 0;
     for (var i = 0; i < photos.length; i++) {
         if (i % 6 === 0) {
-            makeInlineDeco(elem, singleDecos, { rotate: true });
+            genInlineDeco(singleDecos, { rotate: true });
         }
         else if (i % 3 === 0) {
-            makeInlineDeco(elem, reusableDecos, { margin: true });
+            genInlineDeco(reusableDecos, { margin: true });
         }
         let photoKey = photos[i];
-        let childEl = new CorkboardItem();
-        // childEl.setAttribute("img", `https://picsum.photos/seed/${i + 100}/200/300`);
-        childEl.setAttribute("img", `corkboard-img/${photoKey}.png`);
-        childEl.setAttribute("photo-key", photoKey);
-        childEl.setAttribute("entry-type", "art");
-        if (photoKey.includes("Food")) childEl.type = "food";
-        if (photoKey.includes("Mem")) childEl.type = "memory";
-        if (photoKey.includes("Writing")) {
-            childEl.type = "writing";
-            childEl.setAttribute("entry-type", "writing");
-            childEl.setAttribute("img", `corkboard/writing_thumb.png`);
-        }
         var newPlusOrMinus = Math.random() < 0.5 ? -1 : 1;
         if (plusOrMinus === newPlusOrMinus && pmCount < 2) {
             pmCount++;
@@ -333,28 +315,82 @@ function setupEntries (elem) {
         }
         plusOrMinus = newPlusOrMinus;
 
-        childEl.style = `--angle: ${Math.round((2 + Math.random() * 5) * plusOrMinus)}deg;`;
-        childEl.style.top = `${Math.round((8 + Math.random() * 25 * plusOrMinus))}px`
-        childEl.style.left = `${Math.round((8 + Math.random() * 25 * plusOrMinus))}px`
-        childEl.style.rotate = `${Math.round((2 + Math.random() * 5 * plusOrMinus))}deg`
-        childEl.style.zIndex = `${100 - i}`;
-
-        elem.append(childEl);
-        // elem.observer.observe(childEl);
+        let data = {
+            type: "corkboard-item",
+            photoKey: photoKey,
+            path: `corkboard-img/${photoKey}.png`,
+            rotate: Math.round((2 + Math.random() * 5 * plusOrMinus)),
+            zIndex: 100 + photos.length - i
+        }
+        if (photoKey.includes("Food")) data.type = "food";
+        if (photoKey.includes("Mem")) data.type = "memory";
+        if (photoKey.includes("Writing")) {
+            data.type = "writing";
+        }
+        randomData.push(data);
     }
 }
 
-function makeInlineDeco (elem, decos, options) {
-    let decoEl = document.createElement("img");
-    decoEl.classList.add("corkboard-item", "inline-deco")
+function setupEntries (elem, entryData) {
+    entryData.forEach((data) => {
+        if (data.type === "inline-deco") {
+            makeInlineDeco(elem, data);
+        }
+        else {
+            let childEl = new CorkboardItem();
+            childEl.type = data.type;
+            childEl.setAttribute("img", data.path);
+            childEl.setAttribute("photo-key", data.photoKey);
+            childEl.setAttribute("entry-type", "art");
+            if (data.type == "writing") {
+                childEl.setAttribute("entry-type", "writing");
+                childEl.setAttribute("img", `corkboard/writing_thumb.png`);
+            }
+            if (data.ratio) {
+                childEl.style.setProperty('aspect-ratio', data.ratio.toFixed(2));
+            }
+            childEl.style.rotate = `${data.rotate}deg`
+            childEl.style.zIndex = `${data.zIndex}`;
+            elem.append(childEl);
+            elem.observer.observe(childEl);
+        }
+    })
+}
+
+function genInlineDeco (decos, options) {
     let randomDecoPath = decos[Math.floor(Math.random() * decos.length)];
-    decoEl.src = `/image-resize/full/${randomDecoPath}`;
+    let data = {
+        type: "inline-deco",
+        path: randomDecoPath,
+    };
     let plusOrMinus = Math.random() < 0.5 ? -1 : 1;
     if (options.rotate) {
-        decoEl.style.rotate = `${Math.round((2 + Math.random() * 5 * plusOrMinus))}deg`
+        data.rotate = Math.round((2 + Math.random() * 5 * plusOrMinus));
     }
     if (options.margin) {
-        decoEl.style.margin = `1vw ${Math.round((2 + Math.random() * 5))}vw`
+        data.margin = Math.round((2 + Math.random() * 5));
+    }
+    randomData.push(data);
+}
+function makeInlineDeco (elem, data) {
+    let decoEl = document.createElement("img");
+    decoEl.classList.add("corkboard-item", "inline-deco")
+    decoEl.src = `/image-resize/full/${data.path}`;
+    if (SETUP) {
+        decoEl.addEventListener("load", function (e) {
+            let ratio = e.target.naturalWidth / e.target.naturalHeight;
+            let idx = Array.from(this.parentNode.children).indexOf(this);
+            randomData[idx].ratio = ratio;
+        }.bind(decoEl))
+    }
+    if (data.ratio) {
+        decoEl.style.setProperty('aspect-ratio', data.ratio.toFixed(2));
+    }
+    if (data.rotate) {
+        decoEl.style.rotate = `${data.rotate}deg`
+    }
+    if (data.margin) {
+        decoEl.style.margin = `1vw ${data.margin}vw`
     }
     elem.append(decoEl);
 }
